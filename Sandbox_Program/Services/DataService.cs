6565ffb8ce029f04455dc2eb2107e58dd3945ea7
kovass04 <kovass04@gmail.com>
2023-08-10 19:58:05 +0300

    Add project files.

---
+++ Sandbox_Program/Services/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Sandbox_Program.Services
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Sandbox_Program/Views/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Windows.UI.Xaml.Controls;
+using Sandbox_Program.Services;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Sandbox_Program
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly DataService _Services;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _Services = new DataService();
+            _ = Loadss();
+        }
+
+        async Task Loadss()
+        {
+            //_ = await _Services.GetLanguagesAsync();
+        }
+    }
+}
