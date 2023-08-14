238e14a2570f5268df839d1c112d93dd8349bae8
kovass04 <kovass04@gmail.com>
2023-08-14 20:54:26 +0300

    add Problem model and rewrite LanguageOption

---
+++ Sandbox_Program/Models/LanguageOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox_Program.Models
+{
+    public class LanguageOption
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string BaseCode { get; set; }
+    }
+}
---
+++ Sandbox_Program/Models/Problem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox_Program.Models
+{
+    public class Problem
+    {
+        public string SampleOutput { get; set; }
+        public string SampleInput { get; set; }
+        public string Detail { get; set; }
+
+        public string Name { get; set; }
+    }
+}
